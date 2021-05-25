9b70d29044141cf2d0b3dca5e92241e005c18614
Kowndinya Boyalakuntla <46968076+Kowndinya2000@users.noreply.github.com>
2021-05-25 17:40:45 +0530

    update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ let upload = document.querySelector(".upload");
 let uploadBtn = document.querySelector(".upload__button");
 uploadBtn.addEventListener("click", async () => {
     upload.classList.add("uploading");
-    await sleep(3000);
-    upload.classList.add("uploaded");
     await sleep(2000);
+    upload.classList.add("uploaded");
+    await sleep(1000);
     upload.classList.remove("uploading");
     upload.classList.add("uploaded-after");
-    await sleep(1000);
+    await sleep(500);
     upload.className = "upload";
     upload_file()
 });
